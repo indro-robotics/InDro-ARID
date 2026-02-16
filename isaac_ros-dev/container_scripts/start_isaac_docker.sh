b3d80f54e488ead197c8e298b55b7dc7b2dbba7f
Calvin-InDro <calvin.rubens@indrorobotics.com>
2026-02-16 20:54:57 +0000

    fully fuctional container

--- isaac_ros-dev/container_scripts/start_isaac_docker.sh
+++ isaac_ros-dev/container_scripts/start_isaac_docker.sh
@@ -4,4 +4,4 @@ set -e
 CONTAINER=isaac_ros_dev-aarch64-container
 
 echo "/// Starting container ${CONTAINER}... ///"
-docker start "${CONTAINER}" >/dev/null 2>&1 || true
+docker start "${CONTAINER}"
